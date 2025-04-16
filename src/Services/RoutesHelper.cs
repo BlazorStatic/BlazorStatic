633775574a36a13d46d56883b00fbf8b79f39872
MeltyObserver <164113402+MeltyObserver@users.noreply.github.com>
2025-04-17 00:48:33 +0100

    add comments to the RoutesHelper class

--- src/Services/RoutesHelper.cs
+++ src/Services/RoutesHelper.cs
@@ -3,36 +3,44 @@ using Microsoft.AspNetCore.Components;
 
 namespace BlazorStatic.Services;
 
-// borrowed code with minor changes
-// https://andrewlock.net/finding-all-routable-components-in-a-webassembly-app/
+/// <summary>
+/// RoutesHelper is responsible for extracting static, non-parameterized routes from Blazor components within a given assembly. <br />
+/// code is borrowed with some changes from:
+/// https://andrewlock.net/finding-all-routable-components-in-a-webassembly-app/
+/// </summary>
 internal static class RoutesHelper
 {
     /// <summary>
     ///     Gets the static routes of a blazor app
     /// </summary>
     /// <param name="assembly">assembly of the blazor app</param>
-    /// <returns>a list of static routes</returns>
+    /// <returns>An array of static routes</returns>
     public static string[] GetRoutesToRender(Assembly assembly)
     {
         return assembly.ExportedTypes
             .Where(t => t.IsSubclassOf(typeof(ComponentBase)))
-            .SelectMany(GetRouteFromComponent)
+            .SelectMany(GetRoutesFromComponent)
             .OfType<string>()
             .ToArray();
     }
 
     /// <summary>
-    ///     Gets the routes from a blazor component
+    ///     Gets the static routes of a blazor component
     /// </summary>
     /// <param name="component"></param>
-    /// <returns>The routes of the component.</returns>
-    private static string[] GetRouteFromComponent(Type component)
+    /// <returns>
+    ///     An array of static routes. <br />
+    ///     Array values can contain null. <br />
+    ///     Returns an empty array if the component doesn't have `@page` directive.
+    /// </returns>
+    private static string[] GetRoutesFromComponent(Type component)
     {
         var attributes = component.GetCustomAttributes(typeof(RouteAttribute), inherit: false);
         var routes = new string[attributes.Length];
         for(int i = 0; i < attributes.Length; i++)
         {
             var attr = (RouteAttribute)attributes[i];
+            // Ignore parameterized routes (e.g /{Id}) because we can't generate them.
             if(!attr.Template.Contains('{'))
             {
                 routes[i] = attr.Template;

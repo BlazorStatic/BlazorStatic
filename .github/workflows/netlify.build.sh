08c157be1eccdfef5a18b7e1d5ba7ecc204dd9f0
tesar-tech <jan@tesar.tech>
2025-11-26 16:42:00 +0100

    netlify build

--- .github/workflows/netlify.build.sh
+++ .github/workflows/netlify.build.sh
@@ -5,14 +5,14 @@ set -e
 pushd /tmp
 wget https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh
 chmod u+x /tmp/dotnet-install.sh
-/tmp/dotnet-install.sh --channel 9.0
+/tmp/dotnet-install.sh --channel 10.0
 
 ## install and run tailwind
 wget https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
 chmod +x /tmp/tailwindcss-linux-x64
 popd
 pushd BlazorStaticWebsite
-/tmp/tailwindcss-linux-x64 --input ./wwwroot/app.css  --output ./wwwroot/app.min.css  --minify
+/tmp/tailwindcss-linux-x64 --input ./wwwroot/app.css --output ./wwwroot/app.min.css --minify
 popd
 
 ## run the project to build the static files. Ensure launch profile ASPNETCORE_ENVIRONMENT is set to Release!
